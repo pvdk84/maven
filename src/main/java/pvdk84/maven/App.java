120374bd8c0a2030d6f35cd4aba68afa968bb35a
pvdk84 <pm.vanderkolk@gmail.com>
2018-11-09 14:28:03 +0100

    first commit maven project

---
+++ src/main/java/pvdk84/maven/App.java
@@ -0,0 +1,13 @@
+package pvdk84.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
